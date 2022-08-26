6288ce983ca4b7bac3c9d99a7226dd89e6bcaf45
91093861 <91093861@mvusd.net>
2022-08-26 12:43:29 -0700

    bug when trying to open Intro screen

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.service.autofill.OnClickAction;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
         //making button move to Intro screen
         introBtn = (Button) findViewById(R.id.intro);
-        introBtn.setOnClickListener(new View.OnClickListener() {
+        introBtn.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View view) {
                 openActivityI();
@@ -30,7 +30,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void openActivityI()
     {
-        Intent intent = new Intent(this, Introduction.class);
-        startActivity(intent);
+        Button introBtn = (Button) findViewById(R.id.intro);
+        introBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v)
+            {
+                startActivity(new Intent(MainActivity.this, Intro.class));
+            } // end onCLick
+        });
     }
 }
