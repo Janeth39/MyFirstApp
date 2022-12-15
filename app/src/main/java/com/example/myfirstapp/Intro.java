1e8b7dc3112ac731649759320a70c36e085b5925
Home <91093861@mvusd.net>
2022-12-14 19:25:02 -0800

    finished the last touches

--- app/src/main/java/com/example/myfirstapp/Intro.java
+++ app/src/main/java/com/example/myfirstapp/Intro.java
@@ -1,14 +1,44 @@
 package com.example.myfirstapp;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Intro extends AppCompatActivity {
     @Override
-    protected void onCreate( Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro);
 
+        //making button move to Intro screen
+        Button introBtn = (Button) findViewById(R.id.nextbtn);
+
+        introBtn.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View view) {
+                openActivityI();
+            }
+        });
+
+    }
+
+    public void openActivityI()
+    {
+        Button introBtn = (Button) findViewById(R.id.nextbtn);
+        introBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v)
+            {
+                startActivity(new Intent(Intro.this, TopFive.class));
+                Toast.makeText( null ,"Opening Top Five Page", Toast.LENGTH_LONG).show();
+
+            } // end onCLick
+        });
     }
 }
--- app/src/main/java/com/example/myfirstapp/TopFive.java
+++ app/src/main/java/com/example/myfirstapp/TopFive.java
@@ -1,14 +1,35 @@
 package com.example.myfirstapp;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class TopFive extends AppCompatActivity {
     @Override
-    protected void onCreate( Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.intro);
+        setContentView(R.layout.top_five);
 
+        //making button move to Intro screen
+        Button HomeBtn = (Button) findViewById(R.id.Home);
+
+        HomeBtn.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View view) {
+                openActivityI();
+            }
+        });
+
+    }
+
+    public void openActivityI()
+    {
+                startActivity(new Intent(this, MainActivity.class));
     }
 }
